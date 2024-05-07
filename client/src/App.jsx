67ae03a89bd0241ab28d7d27884538542ae541c6
Deepumehra <deepumehra1729@gmail.com>
2024-05-07 15:04:23 +0530

    Basic server and client created

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <h1 className=' flex align-center justify-center bg-red-500 500 text-white'>
+      LMS
+    </h1>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,36 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { config } from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import errorMiddleware from './middlewares/error.middleware.js';
+config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Third-Party
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+// Server Status Check Route
+app.get('/ping', (_req, res) => {
+    res.send('Pong');
+  });
+
+app.all('*', (_req, res) => {
+    res.status(404).send('OOPS!!! 404 Page Not Found');
+});
+  
+// Custom error handling middleware
+app.use(errorMiddleware);
+  
+export default app;
